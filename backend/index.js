dd42b9fbc948a74f7c2e1ae2c19f9b9838712c3a
Sarra Ben Hamida <81316721+sarra19@users.noreply.github.com>
2024-08-11 18:54:17 +0100

    commit

---
+++ backend/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+require('dotenv').config();
+const connectDB = require('./config/db');
+const router = require('./routes');
+
+const app = express();
+
+// CORS configuration
+app.use(cors({
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+}));
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api", router);
+
+// Corrected PORT definition
+const PORT = process.env.PORT || 8080;
+
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log("Connected to DB");
+        console.log("Server is running on port " + PORT);
+    });
+});
